95a82a44a66e48f5908e69f975d54b73be809cd9
Jon Železnik <57703270+jonzeleznik@users.noreply.github.com>
2023-09-20 19:24:49 +0200

    Add: json labels

--- models/tmdbMovieModel.go
+++ models/tmdbMovieModel.go
@@ -1,22 +1,22 @@
 package models
 
 type Movies struct {
-	Adult                 bool
-	Backdrop_path         string
-	Belongs_to_collection []string
-	Budget                int
-	Genres                []Genre
-	Homepage              string
-	Id                    int
-	Imdb_id               string
-	Original_language     string
-	Original_title        string
-	Overview              string
-	Popularity            float64
-	Poster_path           string
-	Release_date          string
-	Title                 string
-	Video                 bool
-	Vote_average          float32
-	Vote_count            int
+	Adult             bool    `json:"adult"`
+	Backdrop_path     string  `json:"backdrop_path"`
+	Budget            int     `json:"budget"`
+	Genres            []Genre `json:"genres"`
+	Homepage          string  `json:"homepage"`
+	Id                int     `json:"id"`
+	Imdb_id           string  `json:"imdb_id"`
+	Original_language string  `json:"original_language"`
+	Original_title    string  `json:"original_title"`
+	Overview          string  `json:"overview"`
+	Popularity        float64 `json:"popularity"`
+	Poster_path       string  `json:"poster_path"`
+	Release_date      string  `json:"release_date"`
+	Runtime           int     `json:"runtime"`
+	Title             string  `json:"title"`
+	Video             bool    `json:"video"`
+	Vote_average      float32 `json:"vote_average"`
+	Vote_count        int     `json:"vote_count"`
 }
